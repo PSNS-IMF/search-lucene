e62b7e7ca8f32009c4610e0e3083e7f073f63d78
Jeff Purcell <japurcell@users.noreply.github.com>
2017-02-21 07:33:16 -0800

    Added function overloads that accept an Analyzer.

--- Psns.Common.Search.Lucene/Compose.cs
+++ Psns.Common.Search.Lucene/Compose.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using LanguageExt;
+using Lucene.Net.Analysis;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.Search;
@@ -18,7 +19,16 @@ namespace Psns.Common.Search.Lucene
         /// <param name="directory">Path to store the index</param>
         /// <returns></returns>
         public static IIndexWriter indexWriterFactory(string directory) =>
-            new LuceneIndexWriter(directory, new LowerCaseKeyWordAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED);
+            indexWriterFactory(directory, new LowerCaseKeyWordAnalyzer());
+
+        /// <summary>
+        /// Create a LuceneIndexWriter with Unlimited MaxFieldLength
+        /// </summary>
+        /// <param name="directory">Path to store the index</param>
+        /// <param name="analyzer">An analyzer to use for converting text into search terms</param>
+        /// <returns></returns>
+        public static IIndexWriter indexWriterFactory(string directory, Analyzer analyzer) =>
+            new LuceneIndexWriter(directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED);
 
         /// <summary>
         /// Set MaxClauseCount to 4096 and FIPSCompliant to true
@@ -33,16 +43,26 @@ namespace Psns.Common.Search.Lucene
         }
 
         /// <summary>
-        /// Perform an action using an index writer that will be disposed when finished
+        /// Perform an action using an index writer (using LowerCaseKeyWordAnalyzer) that will be disposed when finished
         /// </summary>
-        /// <param name="directory"></param>
-        /// <param name="useWriter"></param>
+        /// <param name="directory">The directory where the index is stored</param>
+        /// <param name="useWriter">The function to be provided an index writer</param>
         /// <returns></returns>
         public static Either<Exception, T> tryWithLuceneIndexWriter<T>(string directory, Func<IIndexWriter, T> useWriter) =>
-            tryWithIndexWriter(fun(() => indexWriterFactory(directory)), directory, useWriter);
+            tryWithLuceneIndexWriter(directory, new LowerCaseKeyWordAnalyzer(), useWriter);
 
         /// <summary>
-        /// Rebuild an index using a Lucene IndexWriter
+        /// Perform an action using an index writer that will be disposed when finished
+        /// </summary>
+        /// <param name="directory">The directory where the index is stored</param>
+        /// <param name="analyzer">An analyzer to use for converting text into search terms</param>
+        /// <param name="useWriter">The function to be provided an index writer</param>
+        /// <returns></returns>
+        public static Either<Exception, T> tryWithLuceneIndexWriter<T>(string directory, Analyzer analyzer, Func<IIndexWriter, T> useWriter) =>
+            tryWithIndexWriter(fun(() => indexWriterFactory(directory, analyzer)), directory, useWriter);
+
+        /// <summary>
+        /// Rebuild an index using a Lucene IndexWriter with a LowerCaseKeyWordAnalyzer
         /// </summary>
         /// <param name="directory"></param>
         /// <param name="itemDocumentChunks"></param>
@@ -55,19 +75,51 @@ namespace Psns.Common.Search.Lucene
             IEnumerable<IEnumerable<Tuple<T, Document>>> itemDocumentChunks,
             Func<Tuple<T, Document>, Term> termFactory,
             Action<int> chunkIndexedCallback) =>
-                await rebuildSearchIndexAsync(
-                    itemDocumentChunks, 
-                    fun((Func<IIndexWriter, Unit> useWriter) => tryWithLuceneIndexWriter(directory, useWriter)), 
+                await rebuildSearchIndexWithLuceneIndexWriterAsync(
+                    directory,
+                    new LowerCaseKeyWordAnalyzer(),
+                    itemDocumentChunks,
                     termFactory,
                     chunkIndexedCallback);
 
         /// <summary>
-        /// Increase search speed by reducing index segmentation
+        /// Rebuild an index using a Lucene IndexWriter
         /// </summary>
-        /// <param name="directory"></param>
+        /// <param name="directory">The directory where the index is stored</param>
+        /// <param name="analyzer">An analyzer to use for converting text into search terms</param>
+        /// <param name="itemDocumentChunks"></param>
+        /// <param name="termFactory"></param>
+        /// <param name="chunkIndexedCallback">Will be called after each chunk is indexed providing a count of 
+        /// how many documents are in the chunk</param>
+        /// <returns></returns>
+        public static async Task<Either<Exception, Unit>> rebuildSearchIndexWithLuceneIndexWriterAsync<T>(
+            string directory,
+            Analyzer analyzer,
+            IEnumerable<IEnumerable<Tuple<T, Document>>> itemDocumentChunks,
+            Func<Tuple<T, Document>, Term> termFactory,
+            Action<int> chunkIndexedCallback) =>
+                await rebuildSearchIndexAsync(
+                    itemDocumentChunks,
+                    fun((Func<IIndexWriter, Unit> useWriter) => tryWithLuceneIndexWriter(directory, analyzer, useWriter)),
+                    termFactory,
+                    chunkIndexedCallback);
+
+        /// <summary>
+        /// Increase search speed by reducing index segmentation with a LowerCaseKeyWordAnalyzer
+        /// </summary>
+        /// <param name="directory">The directory where the index is stored</param>
         /// <returns></returns>
         public static async Task<Either<Exception, Unit>> optimizeWithLuceneIndexWriterAsync(string directory) =>
+            await optimizeWithLuceneIndexWriterAsync(directory, new LowerCaseKeyWordAnalyzer());
+
+        /// <summary>
+        /// Increase search speed by reducing index segmentation
+        /// </summary>
+        /// <param name="directory">The directory where the index is stored</param>
+        /// <param name="analyzer">An analyzer to use for converting text into search terms</param>
+        /// <returns></returns>
+        public static async Task<Either<Exception, Unit>> optimizeWithLuceneIndexWriterAsync(string directory, Analyzer analyzer) =>
             await optimizeIndexAsync(
-                fun((Func<IIndexWriter, Unit> useWriter) => tryWithLuceneIndexWriter(directory, useWriter)));
+                fun((Func<IIndexWriter, Unit> useWriter) => tryWithLuceneIndexWriter(directory, analyzer, useWriter)));
     }
 }
--- Psns.Common.Search.Lucene/LowerCaseKeyWordAnalyzer.cs
+++ Psns.Common.Search.Lucene/LowerCaseKeyWordAnalyzer.cs
@@ -3,7 +3,7 @@ using Lucene.Net.Analysis;
 
 namespace Psns.Common.Search.Lucene
 {
-    internal class LowerCaseKeyWordAnalyzer : Analyzer
+    public class LowerCaseKeyWordAnalyzer : Analyzer
     {
         public override TokenStream TokenStream(string fieldName, TextReader reader)
         {
