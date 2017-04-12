5ec4da21ca1eea069643b444f271f599b5ed9bc2
Jeff Purcell <japurcell@users.noreply.github.com>
2017-04-12 13:09:55 -0700

    Expose addIndexexNoOptimize on LuceneIndexWriter

--- Psns.Common.Search.Lucene/IIndexWriter.cs
+++ Psns.Common.Search.Lucene/IIndexWriter.cs
@@ -21,6 +21,20 @@ namespace Psns.Common.Search.Lucene
         /// <param name="doc"></param>
         void UpdateDocument(Term term, Document doc);
 
+        /// <summary>
+        /// Merges all segments from an array of indexes into this index. 
+        /// This may be used to parallelize batch indexing. A large document 
+        /// collection can be broken into sub-collections. Each sub-collection 
+        /// can be indexed in parallel, on a different thread, process or machine. 
+        /// The complete index can then be created by merging sub-collection 
+        /// indexes with this method. NOTE: the index in each Directory must 
+        /// not be changed (opened by a writer) while this method is running. 
+        /// This method does not acquire a write lock in each input Directory, 
+        /// so it is up to the caller to enforce this.
+        /// </summary>
+        /// <param name="dirs"></param>
+        void AddIndexesNoOptimize(params Directory[] dirs);
+
         void Optimize();
 
         void DeleteAll();
